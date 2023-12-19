38c0f2db043b0f62bd96bafb398291a32bd310bf
Nico Spiller <nspiller1@mailbox.org>
2023-12-19 12:20:20 -0500

    changed matplotlib style handling

--- src/visualize.py
+++ src/visualize.py
@@ -16,50 +16,56 @@ def plot_data(
     figsize=(10, 5),
     palette="tab10",
     palette_vlines="Pastel1",
+    matplotlib_style="default",
 ):
-    fig, ax = plt.subplots(figsize=figsize)
+    
+    with plt.style.context(matplotlib_style):
 
-    cmap = plt.get_cmap(palette)
-    cmap_vlines = plt.get_cmap(palette_vlines)
+        fig, ax = plt.subplots(figsize=figsize)
 
-    i, j = 0, 0
-    ylabels = []
+        cmap = plt.get_cmap(palette)
+        cmap_vlines = plt.get_cmap(palette_vlines)
 
-    for name in data:
-        time_events = data[name]
-        if t_cutoff is not None:
-            mask = time_events < t_cutoff
-            time_events = time_events[mask]
+        i, j = 0, 0
+        ylabels = []
 
-        if name in key_vlines:
-            label = name
-            for t in time_events:
-                ax.axvline(
-                    t, color=cmap_vlines(j % cmap_vlines.N), label=label, zorder=99
+        for name in data:
+            time_events = np.array(data[name])
+            if t_cutoff is not None:
+                mask = time_events < t_cutoff
+                time_events = time_events[mask]
+
+            if name in key_vlines:
+                label = name
+                for t in time_events:
+                    ax.axvline(
+                        t, color=cmap_vlines(j % cmap_vlines.N), label=label, zorder=99
+                    )
+                    if label is not None:
+                        j += 1
+                        label = None
+                
+            else:
+                ax.eventplot(
+                    time_events,
+                    lineoffsets=i,
+                    linewidth=linewidth,
+                    linelengths=linelengths,
+                    colors=cmap(i % cmap.N),
                 )
-                label = None
-            j += 1
-        else:
-            ax.eventplot(
-                time_events,
-                lineoffsets=i,
-                linewidth=linewidth,
-                linelengths=linelengths,
-                colors=cmap(i % cmap.N),
-            )
-            i += 1
-            ylabels.append(name)
+                i += 1
+                ylabels.append(name)
 
-    ax.set_xlim(xlims)
-    ax.set_xlabel(xlabel)
-    ax.set_ylim(-0.5, len(ylabels) - 0.5)
+        ax.set_xlim(xlims)
+        ax.set_xlabel(xlabel)
+        ax.set_ylim(-0.5, len(ylabels) - 0.5)
 
-    ax.set_yticks(range(len(ylabels)))
-    ax.set_yticklabels(ylabels)
-    if j:
-        ax.legend(loc="upper right")
+        ax.set_yticks(range(len(ylabels)))
+        ax.set_yticklabels(ylabels)
+        if j:
+            ax.legend(loc="upper right")
 
-    return fig, ax
+        return fig, ax
 
 
 def hide_axes_and_legend(ax):
@@ -78,6 +84,11 @@ def hide_axes_and_legend(ax):
     ax.set_xlabel("")
     ax.set_axis_off()
 
+    try:
+        ax.get_legend().set_visible(False)
+    except AttributeError:
+        pass
+
 
 def draw_time_indicator(ax, time_point, color="red"):
     ax.axvline(time_point, color=color, zorder=99)
@@ -92,28 +103,28 @@ def convert_figure_to_image(fig, dpi):
     return frame
 
 
-def draw_frame(data, time_point, t_max, plot_params, dpi, matplotlib_style):
-    with plt.style.context(matplotlib_style):
-        fig, ax = plot_data(data, t_cutoff=time_point, xlims=(0, t_max), **plot_params)
+def draw_frame(data, time_point, t_max, plot_params, dpi, key_vlines):
 
-        draw_time_indicator(ax, time_point=time_point)
-        hide_axes_and_legend(ax)
+    fig, ax = plot_data(data, key_vlines=key_vlines, t_cutoff=time_point, xlims=(0, t_max), **plot_params)
 
-        frame = convert_figure_to_image(fig, dpi=dpi)
-        plt.close(fig)
+    draw_time_indicator(ax, time_point=time_point)
+    hide_axes_and_legend(ax)
 
-        return frame
+    frame = convert_figure_to_image(fig, dpi=dpi)
+    plt.close(fig)
+
+    return frame
 
 
 def generate_movie(
-    data, time_resolution, plot_params, path_movie, dpi=300, matplotlib_style="classic"
+    data, time_resolution, plot_params, path_movie, dpi=300, key_vlines=[], n_jobs=-1,
 ):
     t_max = np.concatenate([*data.values()]).max()
     time_points = np.arange(0, t_max, time_resolution)
 
-    with parallel_backend("loky", n_jobs=-1):
+    with parallel_backend("loky", n_jobs=n_jobs):
         frames = Parallel()(
-            delayed(draw_frame)(data, t, t_max, plot_params, dpi, matplotlib_style)
+            delayed(draw_frame)(data, t, t_max, plot_params, dpi, key_vlines)
             for t in time_points
         )
 
